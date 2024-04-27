0827e2e8fba553021469691ff29255c451a3ba6f
Alexey Belous <viu@bk.ru>
2024-04-27 15:11:39 +0300

    2

--- src/space/date/users.java
+++ src/space/date/users.java
@@ -2,9 +2,9 @@ public class users {
 
     //Fortest1
     String user1 = "Fortest1";
-    String passUser1 = 'Sh273Ht1';
+    String passUser1 = "Sh273Ht1";
 
-    //Fortest1
+    //Fortest2
     String user2 = "Fortest2";
-    String passUser2 = 'Sh273Ht1';
+    String passUser2 = "Sh273Ht1";
 }
