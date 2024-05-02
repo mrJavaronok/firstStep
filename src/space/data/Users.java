94a2526e0da9923945cd8c357073abf0a9b117ba
Alexey Belous <viu@bk.ru>
2024-05-02 17:57:03 +0300

    rename classes

---
+++ src/space/data/Users.java
@@ -0,0 +1,28 @@
+import java.util.Objects;
+
+public class Users {
+
+
+    private String name;
+    private String login;
+    private String password;
+
+    public Users(String name, String login, String password) {
+        this.name = name;
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+}
